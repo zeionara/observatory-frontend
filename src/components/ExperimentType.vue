e29bb769a7c8e76fad13614a0df5f71a8e676c14
Dmitrii <zeionara@gmail.com>
2020-12-27 21:06:37 +0300

    Implemented experiment token output, fixed material icons

--- src/components/ExperimentType.vue
+++ src/components/ExperimentType.vue
@@ -1,10 +1,14 @@
 <template>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined">
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;900&display=swap" rel="stylesheet">
   <div class="experiment-type" style="display: flex; flex-direction: column; flex: 1">
     <it-tabs style="flex: 1">
       <it-tab title="Create">
         <div class="param-form">
           <it-input v-for="(paramName, index) in description.params" :key="index" v-model="description.values[paramName]" :message="paramName" class="param-input-field"/>
           <it-button type="primary" pulse outlined class="start-experiment-button" v-on:click="startExperiment()">Start</it-button>
+          <it-progressbar id="sign-in-progress-bar" :style="'display: ' + (isProgressBarVisible ? 'block' : 'none')" infinite />
+          <it-alert type="success" title="Experiment was created successfully" :body="'Experiment token is ' + token" :style="'display: ' + (isExperimentAlertVisible ? 'block' : 'none')"/>
         </div>
       </it-tab>
       <it-tab title="Track">Second tab</it-tab>
@@ -26,14 +30,21 @@ import logging from '@/utils/logging'
 })
 export default class ExperimentType extends Vue {
   description!: ExperimentTypeDescription
+  isProgressBarVisible = false
+  isExperimentAlertVisible = false
+  token = ""
   
   startExperiment() {
     console.log("Starting a new experiment...")
+    this.isProgressBarVisible = true
     queries.post('start-experiment', Object.assign(this.description.values, {'type': this.description.name}))
     .then(response => {
       logging.logObject('Got response:', response)
-      const token = response.data?.token
+      this.token = response.data ? response.data['experiment-id'] : "none"
+      this.isProgressBarVisible = false
+      this.isExperimentAlertVisible = true
     }, error => {
+      this.isProgressBarVisible = false
       logging.logObject('Cannot start an experiment:', error)
     })
   }
